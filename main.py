eeefcf576e48610abb36a38be84d9dfba5272914
Joshua Mutavi <alolagamerza@gmail.com>
2026-09-12 14:35:33 -0500

    Add face landmark tracking

--- main.py
+++ main.py
@@ -52,3 +52,10 @@ class ExpressionReaderApp(ctk.CTk):
         self.cap = cv2.VideoCapture(0)
         self.last_label = None
         self._update_frame()
+
+    def _update_frame(self):
+        ok, frame = self.cap.read()
+        if ok:
+            frame = cv2.flip(frame, 1)
+            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = self.face_mesh.process(rgb)
